8a335c32daf01aaa1655338be99f4f7bb6c0e57c
Tushar Kapila <tgkprog@gmail.com>
2023-04-26 05:29:45 +0530

    alert

--- src/components/SimpleAlert.tsx
+++ src/components/SimpleAlert.tsx
@@ -1,20 +1,64 @@
-import React, { ReactNode } from "react";
+import React, { useEffect, ReactNode, useState } from "react";
 
-export enum AlertType {
-  success,
-  danger,
-  warning,
-  info,
-}
 export interface Props {
   children: ReactNode;
-  alertType: AlertType;
+  alertType?: "success" | "warning" | "danger" | "info";
+  autoHide?: boolean;
+  autoHideMilliSecs?: number;
+  onClose?: () => void;
 }
-export const SimpleAlert = ({ children, alertType }: Props) => {
-  const alerClzStr = "alert alert-" + AlertType[alertType];
+
+export const SimpleAlert = ({
+  children,
+  alertType = "success",
+  autoHide = false,
+  autoHideMilliSecs = 8000,
+  onClose = undefined,
+}: Props) => {
+  //AlertType[alertType] +
+  const alerClzStr =
+    "alert alert-" + alertType + " alert-dismissible fade show";
+  const [show, setShow] = useState(true);
+
+  if (autoHide) {
+    useEffect(() => {
+      const timeId = setTimeout(() => {
+        if (show) {
+          setShow(false);
+          if (onClose !== undefined) {
+            onClose();
+          }
+        }
+      }, autoHideMilliSecs);
+
+      return () => {
+        clearTimeout(timeId);
+        //setShow(false);
+      };
+    }, []);
+  }
+
+  const onCloseBtn = () => {
+    setShow(false);
+    if (onClose !== undefined) {
+      onClose();
+    }
+  };
+  // If show is false the component will return null and stop here
+  if (!show) {
+    return null;
+  }
+
   return (
     <div className={alerClzStr} role="alert">
       {children}
+      <button
+        type="button"
+        data-bs-dismiss="alert"
+        className="btn-close"
+        aria-label="Close"
+        onClick={onCloseBtn}
+      ></button>
     </div>
   );
 };
